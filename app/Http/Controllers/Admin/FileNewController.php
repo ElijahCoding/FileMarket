79f9c7218deed9491bae6542c68eab1676761150
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-16 21:04:31 +0300

    Showing new files for approval

---
+++ app/Http/Controllers/Admin/FileNewController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\File;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FileNewController extends Controller
+{
+    public function index()
+    {
+      $files = File::unapproved()->finished()->oldest()->get();
+
+      return view('admin.files.new.index', [
+        'files' => $files
+      ]);
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
--- resources/views/admin/layouts/partials/_navigation.blade.php
+++ resources/views/admin/layouts/partials/_navigation.blade.php
@@ -2,7 +2,7 @@
     <p class="menu-label">Manage files</p>
     <ul class="menu-list">
         <li>
-            <a href="#">Approve new files</a>
+            <a href="{{ route('admin.files.new.index') }}">Approve new files</a>
         </li>
         <li>
             <a href="#">Approve updated files</a>
