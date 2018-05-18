31a68a5760418258ff852bc0de0132a2cb525726
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-18 13:14:28 +0300

    Sale setup

--- app/File.php
+++ app/File.php
@@ -137,4 +137,9 @@ class File extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -43,4 +43,8 @@ class User extends Authenticatable
       return $this->hasMany(File::class);
     }
 
+    public function sales()
+    {
+      return $this->hasMany(Sale::class);
+    }
 }
